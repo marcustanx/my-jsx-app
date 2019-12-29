cc11e86c6a0041f162156e22f07d92ced7ce8d63
Marcus Tan <marcus@mtss.dev>
2019-12-29 17:33:27 +0800

    .

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { injectBabelPlugin } = require("react-app-rewired");
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(["style-jsx/babel"], config);
-
-  return config;
-};
